7e65233f75a45cd9aee2a8db369886833fceb600
mycoredy <1102768969@qq.com>
2014-12-29 19:52:49 +0800

    resourse

---
+++ resourse/main.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include "../head/EatPatch.h"
+#include "../head/Cur_conver.h"
+#include "../head/Op_longint.h"
+using namespace std;
+int main ()
+{
+/*	Conver c1;
+	c1.eve_converstack();
+	c1.arr_conver();*/
+
+
+//测试长整数运算
+              int a[2]={1204,1024};
+              int b[2]={2048,2048};
+              Node a[2];
+              Node b[2];
+              
+
+
+	return 0;
+}
